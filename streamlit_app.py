bebfdb4e46434f9537a55e2ada71fda6e5d71824
strstudev <58146909+strstudev@users.noreply.github.com>
2023-03-16 19:11:16 +0100

    Update streamlit_app.py
    nouvel header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
