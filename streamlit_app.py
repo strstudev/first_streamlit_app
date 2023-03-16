1c7b1948c8736f14e5dec44e19c6afda97ac1d65
strstudev <58146909+strstudev@users.noreply.github.com>
2023-03-16 19:15:25 +0100

    Update streamlit_app.py
    correction bug titre

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
-streamlit.header('Breakfast Favorites')
+streamlit.header('Déjeuner bon pour la santé')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
