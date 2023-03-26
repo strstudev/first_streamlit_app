02687c3cdad5186c0af7778cd366ac36efd280a0
strstudev <58146909+strstudev@users.noreply.github.com>
2023-03-26 12:09:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
+    streamlit.write('The user entered ', fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
     # take the json version of the response and normalize it
@@ -37,10 +38,6 @@ try:
 
 except URLError as e:
   streamlit.error()
-
-streamlit.write('The user entered ', fruit_choice)
-
- 
 
 streamlit.stop()
 
