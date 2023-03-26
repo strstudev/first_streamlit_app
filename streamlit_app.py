6c6830e4f9fc8543b890c321a196e23a0c8e20f5
strstudev <58146909+strstudev@users.noreply.github.com>
2023-03-26 12:42:21 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ streamlit.stop()
 #streamlit.dataframe(my_data_rows)
 
 #Snowflake-related functions
-def get_fruit_load_list()
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
